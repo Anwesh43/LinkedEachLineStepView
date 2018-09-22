58ed48f405047cfd69835ece40ca5e8269172adb
Anwesh43 <anweshthecool0@gmail.com>
2018-09-22 12:42:15 +0530

    adding view in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedeachlinestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedeachlinestepview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.linkedeachlinestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.eachlinestepview.EachLineStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        EachLineStepView.create(this)
     }
 }
