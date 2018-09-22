74f79e04739b185b5f0e4d92808eed61b2bf5813
Anwesh43 <anweshthecool0@gmail.com>
2018-09-22 12:27:46 +0530

    adding ELSNode class

--- eachlinestepview/src/main/java/com/anwesh/uiprojects/eachlinestepview/EachLineStepView.kt
+++ eachlinestepview/src/main/java/com/anwesh/uiprojects/eachlinestepview/EachLineStepView.kt
@@ -99,4 +99,45 @@ class EachLineStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ELSNode(var i : Int, val state : State = State()) {
+
+        private var prev : ELSNode? = null
+        private var next : ELSNode? = null
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = ELSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ELSNode {
+            var curr : ELSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawELSNode(i, state.scale, paint)
+            prev?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+    }
 }
