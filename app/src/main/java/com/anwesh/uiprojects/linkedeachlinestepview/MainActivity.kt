e19f0f32ed74356c6de5e0060bbb4bda0d496c3c
Anwesh43 <anweshthecool0@gmail.com>
2018-09-22 17:43:56 +0530

    adding Listener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedeachlinestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedeachlinestepview/MainActivity.kt
@@ -3,14 +3,20 @@ package com.anwesh.uiprojects.linkedeachlinestepview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.eachlinestepview.EachLineStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        EachLineStepView.create(this)
+        val view : EachLineStepView = EachLineStepView.create(this)
         fullScreen()
+        view.addEachLineAnimationListener({i -> createToast("animation number ${i + 1} is complete")}, {i -> createToast("animation number ${i + 1} is reset")})
+    }
+
+    private fun createToast(text : String) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show()
     }
 }
 
