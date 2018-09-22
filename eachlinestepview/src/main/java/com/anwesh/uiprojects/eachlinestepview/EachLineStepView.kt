a65f7ca5d1beea09b3130d64a12a1aaf7423d097
Anwesh43 <anweshthecool0@gmail.com>
2018-09-22 12:00:29 +0530

    adding libraries and defining nodes

--- eachlinestepview/src/main/java/com/anwesh/uiprojects/eachlinestepview/EachLineStepView.kt
+++ eachlinestepview/src/main/java/com/anwesh/uiprojects/eachlinestepview/EachLineStepView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.eachlinestepview
 /**
  * Created by anweshmishra on 22/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
